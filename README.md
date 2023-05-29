6469bfe9ff96842f9ae66cc4723b590a924fa37c
Fabrizio Ciacchi <fabrizio@atida.com>
2023-05-29 19:01:55 +0200

    Release 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
-# contentful-cli-export
-Backup tool for Contentful
+[![License: MIT](https://img.shields.io/github/license/AtidaTech/contentful-cli-export)](https://opensource.org/licenses/MIT)
+[![npm](https://img.shields.io/npm/v/contentful-cli-export)](https://npmjs.com/package/contentful-cli-export)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/AtidaTech/contentful-cli-export)
+![Downloads](https://img.shields.io/npm/dw/contentful-cli-export)
+![Forks](https://img.shields.io/github/forks/AtidaTech/contentful-cli-export)
+
+# Contentful Export Tool
+
+This tool simplifies exporting data from a Content Management System (CMS) like Contentful. It's easier to use than the actual Contentful CLI export and can also be easily integrated into a CI/CD pipeline such as GitLab or GitHub.
+
+> Note: This is NOT the official Contentful CLI Export tool
+
+<h3>Sponsored by <a href="https://github.com/AtidaTech"><b>Atida</b> <img src="https://avatars.githubusercontent.com/u/127305035?s=200&v=4" width="14px;" alt="Atida" /></a></h3>
+
+<hr />
+
+[✨ Features](#-features) · [💡 Installation](#-installation) · [📟 Example](#-example) · [🎹 Usage](#-usage) · [📅ToDo](#-todo) · [👾Contributors](#-contributors) · [🎩Acknowledgments](#-acknowledgements) · [📄License](#-license)
+
+<hr />
+
+## ✨ Features
+
+- **Ease of Use:** Compared to the actual Contentful CLI, this tool requires less manual configuration and is more straightforward to use.
+- **CI/CD Pipeline Integration:** It is designed to be used as part of a CI/CD pipeline, like GitLab or GitHub. It doesn't require installation in the system, making it portable and easy to run in various environments.
+- **Command Line Parameters:** All necessary parameters can be passed via command line arguments, making it flexible for different use cases.
+- **ZIP Compression:** This tool provides an option to compress the result into a ZIP file. This is particularly useful when exporting large amounts of data or assets, helping to save storage space.
+- **Consistent Naming:** The naming of the output files (JSON, ZIP, and log) are consistent and predictable, making it easier to manage the exported data.
+
+## 💡 Installation
+
+To use this helper library, you must have [NodeJS 🔗](https://nodejs.org/) and [npm 🔗](http://npmjs.org) installed.
+
+To install it, simply run:
+
+```shell
+npm install contentful-cli-export --save
+```
+
+Or, if using [yarn 🔗](https://yarnpkg.com/lang/en/):
+
+```shell
+yarn add contentful-cli-export
+```
+
+### Requirements
+
+* `node` >= 14.0.0
+* `npm` >= 8.5.5
+* `contentful-management` >= 7.50.0 
+* [contentful-lib-helpers](https://www.npmjs.com/package/contentful-lib-helpers) >= 1.8.0
+
+### Set-up
+
+To better use the Contentful CLI Export, it is recommended to put the following values in your `.env`/`.env.local` file:
+
+```shell
+CMS_SPACE_ID=<space-id>
+CMS_MANAGEMENT_TOKEN=<management-token>
+```
+
+However, the two values could also be passed as parameters during execution.
+
+Also, it is needed that you create an `export/` folder in the root of your project. It will contain all the exports.
+
+## 📟 Example
+
+The basic command should contain the `from` environment we want to export:
+
+```shell
+npx contentful-cli-export --from "<environment-id>"
+```
+
+A more complex export command could be
+
+```shell
+npx contentful-cli-export --from "<environment-id>" --space-id"<space-id>" --management-token "<management-token>" --compress
+```
+
+This will export data from the specified environment-id, space-id, and management token, and compress the result into a ZIP file.
+
+## 🎹 Usage
+
+This script can be used from the command line and accepts various arguments for customization:
+
+* `--from` or `--environment-id` (MANDATORY): The environment id from which data will be exported.
+* `--space-id`: The Contentful space id.
+* `--management-token` or `--mt`: The Contentful Management Token.
+* `--only-published`: To include only published data.
+* `--download-assets`: To include assets in the exported data.
+* `--verbose`: For verbose logging (useful in CI).
+* `--compress`: To compress the result into a ZIP file.
+* `--export-dir`: To specify a custom directory for the exported data (default is sub-directory `export/` in your project root)
+
+## 📅 Todo
+
+* Add compatibility with official Contentful Export env/settings.
+
+## 👾 Contributors
+
+<table>
+  <tr>
+    <td align="center"><a href="https://github.com/fciacchi"><img src="https://images.weserv.nl/?url=avatars.githubusercontent.com/u/58506?v=4&h=100&w=100&fit=cover&mask=circle&maxage=7d" width="100px;" alt="Fabrizio Ciacchi" /><br /><sub><b>@fciacchi</b></sub></a><br /></td>
+    <td align="center"><a href="https://github.com/psyvic"><img src="https://images.weserv.nl/?url=avatars.githubusercontent.com/u/29251597?v=4&h=100&w=100&fit=cover&mask=circle&maxage=7d" width="100px;" alt="Victor Hugo Aizpuruo" /><br /><sub><b>@psyvic</b></sub></a><br /></td>
+    <td align="center"><a href="https://github.com/aalduz"><img src="https://images.weserv.nl/?url=avatars.githubusercontent.com/u/11409770?v=4&h=100&w=100&fit=cover&mask=circle&maxage=7d" width="100px;" alt="Aldo Fernández" /><br /><sub><b>@aalduz</b></sub></a><br /></td>
+    <td align="center"><a href="https://github.com/leslyto"><img src="https://images.weserv.nl/?url=avatars.githubusercontent.com/u/4264812?v=4&h=100&w=100&fit=cover&mask=circle&maxage=7d" width="100px;" alt="Stefan Stoev" /><br /><sub><b>@leslyto</b></sub></a><br /></td>
+  </tr>
+</table>
+
+However, feel free to open issues or pull requests if you have suggestions or improvements to propose.
+
+## 🎩 Acknowledgements
+
+I would like to express my gratitude to the following parties:
+
+- [Atida 🔗](https://www.atida.com/), the company that has allowed these scripts to be open sourced. Atida is an e-commerce platform that sells beauty and medical products. Their support for open source is greatly appreciated. A special thank to <a href="https://github.com/shoopi"><img src="https://images.weserv.nl/?url=avatars.githubusercontent.com/u/1385372?v=4&h=16&w=16&fit=cover&mask=circle&maxage=7d" width="16px;" alt="Shaya Pourmirza" /> Shaya Pourmirza</a> that has been a great promoter and supporter of this initiative inside the company.
+- [Contentful 🔗](https://www.contentful.com/), for creating their excellent content management platform and the JavaScript CMA SDK that this library is built on. Without their work, this project would not be possible.
+
+Thank you to everyone involved!
+
+## 📄 License
+This project is licensed under the [MIT License](LICENSE)
---
+++ contentful-cli-export.js
@@ -0,0 +1,340 @@
+const DELETE_FOLDER_DELAY = 5000
+
+;(async function main() {
+  try {
+    const dirNamePath = await getDirNamePath()
+    const envValues = await getEnvValues(dirNamePath)
+
+    const cmsSpaceId = envValues?.CMS_SPACE_ID ?? 'placeholder-space-id'
+    const cmsManagementToken =
+      envValues?.CMS_MANAGEMENT_TOKEN ?? 'placeholder-management-token'
+
+    const initialSettings = await parseArguments(
+      dirNamePath,
+      cmsSpaceId,
+      cmsManagementToken
+    )
+
+    console.log(initialSettings)
+
+    const options = await extractOptions(initialSettings)
+
+    console.log(options)
+
+    await performExport(options, initialSettings)
+  } catch (error) {
+    console.error('@@/ERROR:', error)
+  }
+})()
+
+/**
+ * Gets the current directory's path.
+ *
+ * @return {Promise<string>} The path of the current directory.
+ */
+async function getDirNamePath() {
+  const { fileURLToPath } = await import('url')
+  const { dirname } = await import('path')
+
+  const __filename = fileURLToPath(import.meta.url)
+  return dirname(__filename)
+}
+
+/**
+ * Reads environment values from .env files.
+ *
+ * @param {string} dirNamePath - The directory path where the .env files are located.
+ * @return {Promise<object>} The environment values.
+ */
+async function getEnvValues(dirNamePath) {
+  const fileSystem = await import('fs')
+  const dotenv = await import('dotenv')
+
+  // Trying to find out where the user's '.env' and '.env.local' files are
+  const rootBasicExists = fileSystem.existsSync(dirNamePath + '/../../../.env')
+  const rootLocalExists = fileSystem.existsSync(
+    dirNamePath + '/../../../.env.local'
+  )
+  const activeBasicExists = fileSystem.existsSync(dirNamePath + '/.env')
+  const activeLocalExists = fileSystem.existsSync(dirNamePath + '/.env.local')
+
+  // Get the data from DotEnv
+  const rootBasicEnv = rootBasicExists
+    ? dotenv.config({ path: dirNamePath + '/../../../.env' }).parsed
+    : {}
+  const rootLocalEnv = rootLocalExists
+    ? dotenv.config({ path: dirNamePath + '/../../../.env.local' }).parsed
+    : {}
+  const activeBasicEnv = activeBasicExists
+    ? dotenv.config({ path: '.env' }).parsed
+    : {}
+  const activeLocalEnv = activeLocalExists
+    ? dotenv.config({ path: '.env.local' }).parsed
+    : {}
+
+  return {
+    ...rootBasicEnv,
+    ...rootLocalEnv,
+    ...activeBasicEnv,
+    ...activeLocalEnv
+  }
+}
+
+/**
+ * Parses command line arguments and sets default values.
+ *
+ * @param {string} dirNamePath - The directory path where the .env files are located.
+ * @param {string} cmsSpaceId - The CMS Space ID.
+ * @param {string} cmsManagementToken - The CMS Management Token.
+ * @return {Promise<object>} The initial settings.
+ */
+async function parseArguments(dirNamePath, cmsSpaceId, cmsManagementToken) {
+  const minimist = (await import('minimist')).default
+  const fileSystem = await import('fs')
+  const dateFormat = (await import('dateformat')).default
+
+  // Some default variables
+  let environmentId = 'placeholder-environment-id'
+  let shouldIncludeDrafts = true
+  let shouldIncludeAssets = false
+  let isVerbose = false
+  let shouldCompressFolder = false
+  let spaceId = cmsSpaceId
+  let managementToken = cmsManagementToken
+
+  let parsedArgs = minimist(process.argv.slice(2))
+  if (
+    parsedArgs.hasOwnProperty('from') &&
+    parsedArgs.hasOwnProperty('environment-id')
+  ) {
+    console.error(
+      "@@/ERROR: Only one of the two options '--environment-id' or '--from' can be specified"
+    )
+    process.exit(1)
+  }
+
+  if (
+    parsedArgs.hasOwnProperty('from') ||
+    parsedArgs.hasOwnProperty('environment-id')
+  ) {
+    environmentId = parsedArgs.from ?? parsedArgs['environment-id']
+  } else {
+    console.error('@@/ERROR: An environment-id should be specified')
+    process.exit(1)
+  }
+
+  if (parsedArgs.hasOwnProperty('space-id')) {
+    spaceId = parsedArgs['space-id']
+  }
+
+  if (
+    parsedArgs.hasOwnProperty('management-token') &&
+    parsedArgs.hasOwnProperty('mt')
+  ) {
+    console.error(
+      "@@/ERROR: Only one of the two options '--management-token' or '--mt' can be specified"
+    )
+    process.exit(1)
+  }
+
+  if (
+    parsedArgs.hasOwnProperty('management-token') ||
+    parsedArgs.hasOwnProperty('mt')
+  ) {
+    managementToken = parsedArgs['management-token'] ?? parsedArgs['mt']
+  }
+
+  if (parsedArgs.hasOwnProperty('only-published')) {
+    shouldIncludeDrafts = false
+  }
+
+  if (parsedArgs.hasOwnProperty('download-assets')) {
+    shouldIncludeAssets = true
+  }
+
+  if (parsedArgs.hasOwnProperty('verbose')) {
+    isVerbose = true
+  }
+
+  if (parsedArgs.hasOwnProperty('compress')) {
+    shouldCompressFolder = true
+  }
+
+  let destinationFolder = dirNamePath + '/export/'
+  let isDestinationFolderCustom = false
+
+  if (parsedArgs.hasOwnProperty('export-dir')) {
+    destinationFolder = parsedArgs['export-dir']
+    isDestinationFolderCustom = true
+  }
+
+  if (!destinationFolder.endsWith('/')) {
+    destinationFolder += '/'
+  }
+
+  // Create destination folder if not present
+  let destinationFolderExists = fileSystem.existsSync(destinationFolder)
+  if (!isDestinationFolderCustom && !destinationFolderExists) {
+    fileSystem.mkdirSync(destinationFolder)
+  }
+
+  destinationFolderExists = fileSystem.existsSync(destinationFolder)
+  if (!destinationFolderExists || destinationFolder === '/') {
+    console.error(
+      '@@/ERROR: Destination folder does not exist or not accessible!'
+    )
+    process.exit(1)
+  }
+
+  const now = new Date()
+  const currentDate = dateFormat(now, 'yyyy-mm-dd-HH-MM-ss')
+  const defaultExportName = currentDate + '-' + spaceId + '-' + environmentId
+
+  return {
+    spaceId: spaceId,
+    environmentId: environmentId,
+    managementToken: managementToken,
+    includeDrafts: shouldIncludeDrafts,
+    includeAssets: shouldIncludeAssets,
+    isVerbose: isVerbose,
+    shouldCompressFolder: shouldCompressFolder,
+    rootDestinationFolder: destinationFolder,
+    defaultExportName: defaultExportName
+  }
+}
+
+/**
+ * Extracts Contentful exporter options from the initial settings.
+ *
+ * @param {object} initialSettings - The initial settings obtained from command line arguments and .env files.
+ * @return {Promise<object>} The options for performing the export.
+ */
+async function extractOptions(initialSettings) {
+  const contentfulManagement = (await import('contentful-management')).default
+  const lib = await import('contentful-lib-helpers')
+  const fileSystem = await import('fs')
+
+  // Set up filename for export file and log
+  const defaultExportName = initialSettings?.defaultExportName
+  const exportDirname =
+    initialSettings.rootDestinationFolder + defaultExportName + '/'
+
+  fileSystem.mkdirSync(exportDirname)
+
+  let contentFile = defaultExportName + '.json'
+  let logFilePath =
+    (initialSettings?.shouldCompressFolder
+      ? initialSettings.rootDestinationFolder
+      : exportDirname) +
+    defaultExportName +
+    '.log'
+
+  if (
+    !(await lib.getEnvironment(
+      contentfulManagement,
+      initialSettings.managementToken,
+      initialSettings.spaceId,
+      initialSettings.environmentId,
+      0
+    ))
+  ) {
+    console.error(
+      "@@/ERROR: Unable to retrieve Destination environment '" +
+        initialSettings?.environmentId +
+        "'!"
+    )
+    console.error(
+      '@@/ERROR: Could also be that the management token or space-id are invalid.'
+    )
+    process.exit(1)
+  }
+
+  console.log(
+    '##/INFO: Export of space-id "' +
+      initialSettings?.spaceId +
+      '" and environment-id "' +
+      initialSettings?.environmentId +
+      '" started...'
+  )
+  console.log('##/INFO: Using destination folder: ' + exportDirname)
+
+  return {
+    spaceId: initialSettings.spaceId,
+    managementToken: initialSettings.managementToken,
+    environmentId: initialSettings.environmentId,
+    exportDir: exportDirname,
+    contentFile: contentFile,
+    saveFile: true,
+    includeDrafts: initialSettings.includeDrafts,
+    includeArchived: initialSettings.includeDrafts,
+    downloadAssets: initialSettings.includeAssets,
+    errorLogFile: logFilePath,
+    useVerboseRenderer: initialSettings.isVerbose,
+    maxAllowedLimit: 100
+  }
+}
+
+/**
+ * Performs the export based on the provided options.
+ *
+ * @param {object} options - The options for performing the export.
+ * @param {object} initialSettings - The initial settings obtained from command line arguments and .env files.
+ */
+async function performExport(options, initialSettings) {
+  const contentfulExport = (await import('contentful-export')).default
+  const admZip = (await import('adm-zip')).default
+  const fileSystem = await import('fs')
+
+  contentfulExport(options).then(() => {
+    const rootExportFolder = initialSettings?.rootDestinationFolder
+    const defaultExportName = initialSettings?.defaultExportName
+    const destinationFolder = rootExportFolder + defaultExportName + '/'
+    const contentFile = destinationFolder + defaultExportName + '.json'
+    let logFile = destinationFolder + defaultExportName + '.log'
+
+    if (initialSettings?.shouldCompressFolder) {
+      // Zip both the folder and json file
+      if (fileSystem.existsSync(destinationFolder)) {
+        console.log('##/INFO: Assets exported. Creating the ZIP File')
+        let zipFile = rootExportFolder + defaultExportName + '.zip'
+        logFile = rootExportFolder + defaultExportName + '.log'
+
+        const zip = new admZip()
+        zip.addLocalFolder(destinationFolder)
+        zip.writeZip(zipFile, deleteFolderAfterZip(destinationFolder))
+
+        console.log('##/INFO: Export completed')
+        console.log('##/INFO: File Saved at:')
+        console.log('##/INFO: ' + zipFile)
+        console.log('##/INFO: Log file (if present) at:')
+        console.log('##/INFO: ' + logFile)
+      } else {
+        console.error('@@/ERROR: Error happens during ZIP file compression')
+      }
+    } else {
+      console.log('##/INFO: Export completed')
+      console.log('##/INFO: File Saved at:')
+      console.log('##/INFO: ' + contentFile)
+      console.log('##/INFO: Log file (if present) at:')
+      console.log('##/INFO: ' + logFile)
+    }
+  })
+}
+
+/**
+ * Deletes the temporary destination folder after the ZIP file has been created.
+ *
+ * @param {string} destinationFolder - The folder to delete.
+ * @return {Promise<boolean>} The result of the deletion operation.
+ */
+async function deleteFolderAfterZip(destinationFolder) {
+  const fileSystem = await import('fs')
+
+  console.log('Deleting Temporary Destination Folder.... ')
+  setTimeout(() => {
+    // Delete folder and json (leave only the zip file)
+    fileSystem.rmdirSync(destinationFolder, { recursive: true })
+  }, DELETE_FOLDER_DELAY)
+
+  return true
+}
